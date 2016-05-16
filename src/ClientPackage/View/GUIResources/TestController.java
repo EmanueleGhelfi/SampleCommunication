a3c8e6ca43487599a2fd2a17ca8179aa6bbad924
melmello <mellonigiulio@gmail.com>
2016-05-16 17:04:02 +0200

    Added King card

---
+++ src/ClientPackage/View/GUIResources/TestController.java
@@ -0,0 +1,24 @@
+package ClientPackage.View.GUIResources;
+
+import ClientPackage.Controller.ClientController;
+import javafx.fxml.Initializable;
+
+import java.net.URL;
+import java.util.ResourceBundle;
+
+/**
+ * Created by Giulio on 16/05/2016.
+ */
+public class TestController implements Initializable {
+
+    private ClientController clientController;
+
+    public void testAction(){
+        //do something
+    }
+
+    @Override
+    public void initialize(URL location, ResourceBundle resources) {
+
+    }
+}
