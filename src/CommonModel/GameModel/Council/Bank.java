93076b34c070ec97bdadae11e7b55ffcbf7cfdf2
melmello <mellonigiulio@gmail.com>
2016-06-16 10:59:25 +0200

    Added bank (temporary)

---
+++ src/CommonModel/GameModel/Council/Bank.java
@@ -0,0 +1,52 @@
+package CommonModel.GameModel.Council;
+
+import CommonModel.GameModel.Card.SingleCard.PoliticCard.PoliticColor;
+
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.HashMap;
+import java.util.Map;
+
+/**
+ * Created by Giulio on 16/06/2016.
+ */
+public class Bank {
+
+    private HashMap<PoliticColor, ArrayList<Councilor>> hashMapArrayList;
+
+    public Bank() {
+        for (PoliticColor politicColor : PoliticColor.values()) {
+            ArrayList<Councilor> politicColorArrayList = new ArrayList<>();
+            for (int i = 0; i < 4; i++) {
+                politicColorArrayList.add(new Councilor(politicColor));
+            }
+            hashMapArrayList.put(politicColor, politicColorArrayList);
+        }
+    }
+
+    public boolean checkCouncilor(PoliticColor politicColor) {
+        int size = hashMapArrayList.get(politicColor).size();
+        if (size > 0 && size < 4) {
+            return true;
+        }
+        return false;
+    }
+
+    public Councilor getCouncilor(Councilor councilor){
+        hashMapArrayList.get(councilor.getColor()).remove(councilor);
+        return councilor;
+    }
+
+    public void setCouncilor(Councilor councilor){
+        hashMapArrayList.get(councilor.getColor()).add(councilor);
+    }
+
+    public ArrayList<PoliticColor> showCouncilor(){
+        ArrayList<PoliticColor> colorToShow = new ArrayList<>();
+        for (Map.Entry<PoliticColor, ArrayList<Councilor>> entry : hashMapArrayList.entrySet()) {
+            if (entry.getValue().size()>0)
+                colorToShow.add(entry.getKey());
+        }
+        return colorToShow;
+    }
+}
