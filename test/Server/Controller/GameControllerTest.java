e4e992caae0f42563cd982f865d5ac7a13c1ed11
EmanueleGhelfi <manughelfi1994@gmail.com>
2016-07-06 22:47:15 +0200

    Test CLI

--- test/Server/Controller/GameControllerTest.java
+++ test/Server/Controller/GameControllerTest.java
@@ -5,6 +5,7 @@ import CommonModel.GameModel.Market.BuyableWrapper;
 import Server.Model.Game;
 import Server.Model.User;
 import Server.NetworkInterface.Communication.FakeCommunication;
+import org.junit.After;
 import org.junit.Before;
 import org.junit.Test;
 
@@ -28,6 +29,7 @@ public class GameControllerTest {
         user = new User(new FakeCommunication(), GamesManager.getInstance());
         enemy = new User(new FakeCommunication(),GamesManager.getInstance());
         user.setGame(game);
+        user.setUsername("Manu");
         user.setHelpers(5);
         game.addUserToGame(user);
         game.addUserToGame(enemy);
@@ -38,9 +40,33 @@ public class GameControllerTest {
     @Test
     public void testMarket() throws Exception {
         ArrayList<BuyableWrapper> buyableWrappers = new ArrayList<>();
-        buyableWrappers.add(new BuyableWrapper(user.getHelpers().get(0),"Manu"));
+        buyableWrappers.add(new BuyableWrapper(user.getHelpers().get(0),user.getUsername()));
         gameController.onReceiveBuyableObject(buyableWrappers);
 
         assertTrue(game.getMarketList().contains(buyableWrappers.get(0)));
+
+        gameController.onRemoveItem(buyableWrappers.get(0));
+
+        assertFalse(game.getMarketList().contains(buyableWrappers.get(0)));
     }
+
+    @Test
+    public void testBuy(){
+
+        int initHelperSize = enemy.getHelpers().size();
+
+        ArrayList<BuyableWrapper> buyableWrappers = new ArrayList<>();
+        buyableWrappers.add(new BuyableWrapper(user.getHelpers().get(0),user.getUsername()));
+        gameController.onReceiveBuyableObject(buyableWrappers);
+
+        assertTrue(game.getMarketList().contains(buyableWrappers.get(0)));
+
+        gameController.onBuyObject(enemy,buyableWrappers);
+
+        assertFalse(game.getMarketList().contains(buyableWrappers.get(0)));
+
+        assertEquals(initHelperSize+1,enemy.getHelpers().size());
+    }
+
+
 }
